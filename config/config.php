922c0812d860516a6af94e4bed1b05cdc18fd481
Zeigadis <pascal.fortunati@laplateforme.io>
2025-10-01 09:44:08 +0200

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'db' => [
+        'dsn' => 'mysql:host=localhost;dbname=moduleconnexion;charset=utf8mb4',
+        'user' => 'root',
+        'pass' => '',
+    ],
+    'base_url' => '/',
+];
